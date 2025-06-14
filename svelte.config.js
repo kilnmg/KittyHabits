31dced6f15d78977e3a41de474293f6d5b08b24a
kilnmg <163559361+kilnmg@users.noreply.github.com>
2025-06-14 19:48:55 +0200

    switch to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,13 +6,7 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html', // This will make all routes fall back to index.html
-      precompress: false
-    })
+    adapter: adapter()
   }
 };
 
